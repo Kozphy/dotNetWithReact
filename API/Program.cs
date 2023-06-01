8868cd30810231e9f2cc8ccf2cb6eebee1959ea3
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-06-01 23:13:15 +0800

    up

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
-// this will create Kestrel server and read configuration file
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
+// this will create Kestrel server and read configuration file
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 // using Microsoft.EntityFrameworkCore;
 builder.Services.AddSqlServer<DataContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
-
 
 var app = builder.Build();
 
@@ -28,5 +29,20 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Start migration
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    var context = services.GetRequiredService<DataContext>();
+    // because we use async Seed, we need to use MigrateAcync here
+    await context.Database.MigrateAsync();
+    // start seeding data to database table
+    await Seed.SeedData(context);
+}catch(Exception ex){
+    System.Console.WriteLine(ex.Message);
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration.");
+}
 
 app.Run();
