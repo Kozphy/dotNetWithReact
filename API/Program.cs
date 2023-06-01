18e6d1b0b9398fbaeb698ff261314a823414cb9a
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-06-01 20:28:53 +0800

    up

---
+++ API/Program.cs
@@ -0,0 +1,32 @@
+// this will create Kestrel server and read configuration file
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+// using Microsoft.EntityFrameworkCore;
+builder.Services.AddSqlServer<DataContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Activity> Activities  { get; set; }
+}
